069c59c1db2db0e89b1854a6871b8d6b29ab5cb5
61758084 <108806281+61758084@users.noreply.github.com>
2025-02-11 11:53:53 +0800

    Add files via upload

---
+++ 计算器_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef 计算器_PRIVATE_H
+#define 计算器_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"城南2019<4> 老煜"
+#define FILE_VERSION	"1.0"
+#define FILE_DESCRIPTION	"使用C++开发的科学计算器"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	"城南2019<4> 老煜"
+#define ORIGINAL_FILENAME	"计算器.exe"
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	"1.0"
+
+#endif /*计算器_PRIVATE_H*/
